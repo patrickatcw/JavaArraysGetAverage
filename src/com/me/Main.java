6448333dda019b77a20f06f33cbee4b4d6a5c90f
patrickatcw <pj8392@yahoo.com>
2018-03-02 17:45:29 -0500

    initial defined array

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -7,6 +7,14 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        //step 1
+        int[] myIntegers = getIntegers(5); //need to do method for this step 2
+        for(int i = 0; i < myIntegers.length; i++) {
+            System.out.println("Element " + i + ", typed value was " + myIntegers[i]);
+        }
+
+
+
 
     }
 
