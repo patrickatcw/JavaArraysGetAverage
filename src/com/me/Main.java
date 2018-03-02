9ba24f8c30d63a45a742679e10dd528f5eeacd54
patrickatcw <pj8392@yahoo.com>
2018-03-02 17:36:55 -0500

    set up scanner because we are going be averaging numbers

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -1,6 +1,10 @@
 package com.me;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
